83ffecc0f70e8f955a28aece88b521577664c9c1
Evgeniy_Er <5evgen_e@mail.ru>
2021-02-15 17:58:10 +0300

    Update Main.kt
    Stage 2/6: Search across multiple lines and multiple queries to search

--- Main.kt
+++ Main.kt
@@ -1,18 +1,40 @@
 package search
 
 fun main() {
-    val words = readLine()!!.split(" ")
-    val word = readLine()!!
-    var index = -1
-    for (i in words.indices) {
-        if (words[i] == word) {
-            index = i
-            break
-        }
+    println("Enter the number of people:")
+    val qtyOfLines = readLine()!!.toInt()
+    val lines = arrayOfNulls<String>(qtyOfLines)
+
+    println("Enter all people:")
+    for (i in 0 until qtyOfLines) {
+        lines[i] = readLine()!!
     }
-    if (index >= 0) {
-        println(index + 1)
-    } else {
-        println("Not Found")
+
+    println()
+    println("Enter the number of search queries:")
+    val qtyOfSearch = readLine()!!.toInt()
+
+    for (i in 0 until qtyOfSearch) {
+        println()
+        println("Enter data to search people:")
+        val dataToSearch = readLine()!!.toUpperCase()
+
+        println()
+        println("Found people:")
+        var indexes = mutableListOf<Int>()
+        for (j in lines.indices) {
+            if (lines[j]!!.toUpperCase().contains(dataToSearch)) {
+                indexes.add(j)
+            }
+        }
+        if (!indexes.isEmpty()) {
+            for (k in indexes) {
+                println(lines[k])
+            }
+
+        } else {
+            println("No matching people found.")
+        }
+        indexes.clear()
     }
 }
